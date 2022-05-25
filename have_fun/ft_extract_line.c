12b0d7f878211b003ef6dc8ebfed8bf035616ece
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-25 16:35:50 +0200

    add strjoin strlen

--- have_fun/ft_extract_line.c
+++ have_fun/ft_extract_line.c
@@ -9,7 +9,9 @@
 
 int	check_newline(char *line);
 char	*get_next_line(int fd);
-char	*ft_read_line(int fd, char *stash);
+char	*ft_read_line(int fd);
+int	ft_strlen(char *str);
+char	*ft_strjoin(char *buf, char *line);
 
 int	main(int argc, char **argv)
 {
@@ -27,24 +29,39 @@ int	main(int argc, char **argv)
 		printf("%s", readed);
 		if (!readed)
 			break ;
-		free(readed);
-	}
+		free(readed);	
+	}	
+	
 	return (0);
 }
 
 // Ma fonction get next line
 char	*get_next_line(int fd)
 {
-	static char	*line;
-	char		*stash;
+	static char	buf[BUFFER_SIZE + 1];
 	int	readed;
+	char	*line;
 	
 	if (fd < 0 || fd < -1 || readed == -1)
 		return (NULL);
-	readed = 1;
-	ft_read_line(fd, line);
+	line = ft_strjoin(buf, line);
+	while (!check_newline(line) && readed != 0)
+	{
+		line = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+		if (line == NULL)
+			return (NULL);
+		readed = read(fd, line, BUFFER_SIZE);
+		if (readed == 0)
+		{
+			free(line);
+			return (NULL);
+		}
+		line = ft_strjoin(buf, line);
+		line[readed] = '\0';
+	}
 	return (line);
 }
+
 // Check si on rencontr un \n a la fin d une ligne
 int	check_newline(char *line)
 {
@@ -55,28 +72,39 @@ int	check_newline(char *line)
 	{
 		if (line[i] == '\n')
 			return (1);
-		i++;	
+		else 
+			return (0);
+		i++;
 	}
 	return (0);
 }
 
-char	*ft_read_line(int fd, char *stash)
+// Ecrire un strjoin qui va me permettre de deplacer mes lignes dans mon buffer
+char	*ft_strjoin(char *buf, char *line)
 {
-	char	*line;
-	int	readed;
+	int	len;
+	int	i;
+	char	*tab;
 	
-	while (!check_newline(line) != 1 && readed != 0)
+	len = (ft_strlen(buf) + ft_strlen(line));
+	buf = (char *)malloc(sizeof(char) * len + 1);
+	if (!buf)
+		return (NULL);
+	i = 0;
+	while (line[i] != '\0')
 	{
-		line = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-		if (line == NULL)
-			return (NULL);
-		readed = read(fd, line, BUFFER_SIZE);
-		if (readed == 0 && stash == NULL)
-		{
-			free(line);
-			return (NULL);
-		}
-		line[readed] = '\0';
+		buf[i] = line[i];
+		i++;
 	}
-	return (line);
+	return (buf);
+}
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while(str[len] != '\0')
+		len++;
+	return (len);
 }
