d922716e086e5752bc8757146d21e610e68a62c4
Nicolas LORION <nlorion.mail@gmail.com>
2022-05-26 14:11:12 +0200

    add check_line check_last_line functions

--- have_fun/ft_extract_line.c
+++ have_fun/ft_extract_line.c
@@ -7,9 +7,10 @@
 
 // PROTOTYPES
 
-int	check_newline(char *line);
+char	*ft_check_line(char *line);
+char	*ft_check_last_line(char *buf, char *line);
 char	*get_next_line(int fd);
-char	*ft_read_line(int fd);
+char	*ft_read_line(int fd, char *buf, char *line);
 int	ft_strlen(char *str);
 char	*ft_strjoin(char *buf, char *line);
 
@@ -31,7 +32,6 @@ int	main(int argc, char **argv)
 			break ;
 		free(readed);	
 	}	
-	
 	return (0);
 }
 
@@ -42,20 +42,37 @@ char	*get_next_line(int fd)
 	int	readed;
 	char	*line;
 	
-	if (fd < 0 || fd < -1 || readed == -1)
+	if (fd < 0 || fd < -1 || read(fd, buf, BUFFER_SIZE) <= -1)
 		return (NULL);
-	line = ft_strjoin(buf, line);
-	while (!check_newline(line) && readed != 0)
+	line = ft_read_line(fd, buf, line);
+	if (line == NULL)
 	{
-		line = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-		if (line == NULL)
-			return (NULL);
-		readed = read(fd, line, BUFFER_SIZE);
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+char	*ft_read_line(int fd, char *buf, char *line)
+{
+	int	readed;
+
+	readed = 1;
+	line = ft_strjoin(buf, line);
+	while (readed != 0)
+	{
 		if (readed == 0)
+			return (NULL);
+		printf("1 : %s\n", line);
+		readed = read(fd, line, BUFFER_SIZE);
+		if (!line || !buf)
 		{
 			free(line);
 			return (NULL);
 		}
+		ft_check_line(line);
+		ft_check_last_line(buf, line);
+		printf("2 : %s\n", line);
 		line = ft_strjoin(buf, line);
 		line[readed] = '\0';
 	}
@@ -63,20 +80,38 @@ char	*get_next_line(int fd)
 }
 
 // Check si on rencontr un \n a la fin d une ligne
-int	check_newline(char *line)
+char	*ft_check_line(char *line)
 {
 	int	i;
 	
 	i = 0;
-	while (line[i])
-	{
-		if (line[i] == '\n')
-			return (1);
-		else 
-			return (0);
+	while (line[i] && line[i] != '\n')
 		i++;
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_check_last_line(char *buf, char *line)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (line[i] && line[i] == '\n')
+	{
+		i++;
+		j = 0;
+		while (line[i])
+		{
+			buf[j] = line[i];
+			i++;
+			j++;
+		}
+		line[i] = '\0';
 	}
-	return (0);
+	return (line);
 }
 
 // Ecrire un strjoin qui va me permettre de deplacer mes lignes dans mon buffer
@@ -85,26 +120,32 @@ char	*ft_strjoin(char *buf, char *line)
 	int	len;
 	int	i;
 	char	*tab;
-	
+	char	*newstr;
+
+	newstr = NULL;	
 	len = (ft_strlen(buf) + ft_strlen(line));
-	buf = (char *)malloc(sizeof(char) * len + 1);
-	if (!buf)
+	tab = (char *)malloc(len + sizeof(char) + 1);
+	newstr = tab;
+	if (!newstr)
 		return (NULL);
 	i = 0;
+	while (*buf != '\0')
+		*tab++ = *buf++;
 	while (line[i] != '\0')
-	{
-		buf[i] = line[i];
-		i++;
-	}
-	return (buf);
+		*tab++ = *line++;
+	*tab = '\0';
+	return (newstr);
 }
 
 int	ft_strlen(char *str)
 {
-	int	len;
+	int	i;
 
-	len = 0;
-	while(str[len] != '\0')
-		len++;
-	return (len);
+	i = 0;
+	while(str[i] != '\0')
+		i++;
+	return (i);
 }
+
+
+
